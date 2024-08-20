4c57775108250f046aa7de4f973e37a74618ced2
Tiz <46307540+tcalvillo@users.noreply.github.com>
2024-08-20 16:55:20 -0500

    Delete 02_data.tf

--- 02_data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_region" "current" {}
-
-data "aws_caller_identity" "current" {}
-
-data "vault_generic_secret" "aws" {
-  path = "projects/aws/sts/${var.bl}-${var.appstackcode}-${var.environment}"
-}
-
-data "aws_partition" "current" {}
