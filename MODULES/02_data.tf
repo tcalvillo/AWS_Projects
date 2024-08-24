bda6064e6645c2e7dfefef5ca78300d542b2659e
Tiz <46307540+tcalvillo@users.noreply.github.com>
2024-08-24 15:55:48 -0500

    Delete MODULES directory

--- MODULES/02_data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-data "aws_partition" "current" {}
-data "aws_vpcs" "listOfVpcsByAccount" {}
--- MODULES/03_locals.tf
+++
@@ -1,7 +0,0 @@
-
-locals {  
-  region_dict = {
-    us-east-1      = "use1"
-  }
-  shortRegion = local.region_dict[data.aws_region.current.name]
-}
