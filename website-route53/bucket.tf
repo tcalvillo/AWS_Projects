db677dc8abe54182424a765cfe09177057b981db
Tiziano Calvillo <tiziano.calvillo@gmail.com>
2025-10-27 19:24:55 -0500

    Adding website with my domain using Route53

---
+++ website-route53/bucket.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name   # "0199.shop" 
+
+  tags = {
+    Name        = "Tiz Travel Website Bucket"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.bucket.id
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
+}
