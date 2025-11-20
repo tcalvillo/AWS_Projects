957b0f400a25ada347c59babedc4e7abd565472c
Tiz <46307540+tcalvillo@users.noreply.github.com>
2025-11-20 10:41:36 -0600

    Update variables.tf
    environment variables

--- website-route53/variables.tf
+++ website-route53/variables.tf
@@ -2,21 +2,21 @@
 variable "root_domain" {
   description = "The root domain name for the website (no www)"
   type        = string
-  default     = "01899.shop"
+  # NOTE: Use environment variables on your laptop for default value
 }
 
 # Subdomain for the main website
 variable "www_domain" {
   description = "The www domain name"
   type        = string
-  default     = "www.01899.shop"
+  # NOTE: Use environment variables on your laptop for default value
 }
 
 # S3 bucket name
 variable "bucket_name" {
   description = "The name of the S3 bucket for hosting the website"
   type        = string
-  default     = "tiz-travel-website-01899"
+  # NOTE: Use environment variables on your laptop for default value
 }
 
 # TTL for DNS records
