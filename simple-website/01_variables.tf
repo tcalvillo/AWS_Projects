4a5e7956b007b44b552e6b8c61263a8ab4bcd39c
Tiz <46307540+tcalvillo@users.noreply.github.com>
2025-11-20 10:42:48 -0600

    Update 01_variables.tf
    environment variables changes

--- simple-website/01_variables.tf
+++ simple-website/01_variables.tf
@@ -16,11 +16,11 @@ variable "region" {
 variable "account_id" {
   description = "The AWS account ID where resources will be created"
   type        = string
-  default     = "123456789012" # NOTE: I replaced the correct one for security reasons
+  # NOTE: Use environment variables on your laptop for default value
 }
 
 variable "bucket_name" {
     description = "The name of the S3 bucket"
     type = string
-    default = "my-simple-website-bucket"
+    # NOTE: Use environment variables on your laptop for default value
 }
